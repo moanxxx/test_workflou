7c53fd6a1e7da3b438fee07c26921e8524192fc3
mrgan <mrgan12009@gmail.com>
2024-12-27 11:21:44 +0300

    test1

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+import os
 import telebot
 
-# Замените "YOUR_BOT_TOKEN" на токен вашего бота, полученный у BotFather
-BOT_TOKEN = "7738016274:AAG0ujQp7V0wtX_yCJ_0n9GpM9nXwvZd7U0"
+# Получаем токен из переменной окружения
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+if not BOT_TOKEN:
+    raise ValueError("Токен бота не найден. Убедитесь, что BOT_TOKEN добавлен в Secrets.")
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-# Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Привет! Это простой бот, который отвечает только на команду /start.")
 
-# Запуск бота
 if __name__ == "__main__":
     print("Бот запущен и готов к работе.")
     bot.infinity_polling()
